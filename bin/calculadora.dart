a56f2f99ee4be4969408c902345096cdd9f623a2
Djang-o <marxrochascr@gmail.com>
2023-05-22 00:02:57 -0300

    implementando a calculador com if e else

--- bin/calculadora.dart
+++ bin/calculadora.dart
@@ -1,14 +1,18 @@
 import 'dart:io';
 
+import 'package:test/expect.dart';
+
 void main(List<String> args) {
   print("Digite um numero");
   String numA = stdin.readLineSync()!;
   int numberA = converter(numA);
   print("digite outro numero");
   String numB = stdin.readLineSync()!;
+  print("Digite a operacao ex: + - * /");
+  String op = stdin.readLineSync()!;
   int numberB = converter(numB);
-  int resul = calcular(numberA, numberB);
-  print("oi $numberA , $numberB , $resul");
+  int resul = calcular(numberA, numberB, op);
+  print("resultado:  $resul");
 }
 
 converter(String numA) {
@@ -16,7 +20,19 @@ converter(String numA) {
   return numberA;
 }
 
-calcular(int numA, int numB) {
-  int resul = numA + numB;
-  return resul;
+calcular(int numA, int numB, op) {
+  int? resul;
+  if (op == "+") {
+    resul = numA + numB;
+    return resul;
+  } else if (op == "-") {
+    resul = numA - numB;
+    return resul;
+  } else if (op == "*") {
+    resul = numA * numB;
+    return resul;
+  } else if (op == "/") {
+    resul = numA ~/ numB;
+    return resul;
+  }
 }
