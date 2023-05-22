dff10c1c6984e2e66b381b984d0888b40e3135dc
Djang-o <marxrochascr@gmail.com>
2023-05-21 23:49:59 -0300

    Implementacao da funcao converter e funcao calcular

--- bin/calculadora.dart
+++ bin/calculadora.dart
@@ -3,9 +3,20 @@ import 'dart:io';
 void main(List<String> args) {
   print("Digite um numero");
   String numA = stdin.readLineSync()!;
-  int numberA = int.parse(numA);
-  print("digite outr numero");
+  int numberA = converter(numA);
+  print("digite outro numero");
   String numB = stdin.readLineSync()!;
-  int numberB = int.parse(numA);
-  print("oi $numberA");
+  int numberB = converter(numB);
+  int resul = calcular(numberA, numberB);
+  print("oi $numberA , $numberB , $resul");
+}
+
+converter(String numA) {
+  int numberA = int.parse(numA);
+  return numberA;
+}
+
+calcular(int numA, int numB) {
+  int resul = numA + numB;
+  return resul;
 }
